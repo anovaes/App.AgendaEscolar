1d925125a8ed087a375adc1ab92f72e5d3e5157f
Alexandre de Oliveira Novaes <alex.novaes8@gmail.com>
2018-01-15 21:15:14 -0200

    Implementação Menu Flyout

---
+++ App.AgendaEscolar/Converters/FormatDateConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace App.AgendaEscolar.Converters
+{
+    public class FormatDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dataDefault = DateTime.MinValue.ToString("dd/MM/yyyy");
+            try
+            {
+                if (value == null)
+                    return dataDefault;
+
+                var data = (DateTimeOffset)value;
+                return data.ToString("dd/MM/yyyy");
+            }
+            catch (Exception)
+            {
+                return dataDefault;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
